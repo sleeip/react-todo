99786bf70aa9ff64496f9b8467234a7e57bc4c3a
ravi <rafael.vitor@jusbrasil.com.br>
2018-10-30 23:14:32 -0300

    Code cleanup

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,50 +1,45 @@
-import React, { Component } from 'react';
-import TodoList from './TodoList'
+import React, { Component } from "react";
+import TodoList from "./TodoList";
 
 export default class Form extends Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            value: '',
-            globalid: 0,
-            list: []
-        };
-        this.appendToList = this.appendToList.bind(this)
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: "",
+      globalid: 0,
+      list: []
+    };
+  }
 
+  appendToList = (val) => {
+    let stateSave = this.state.list;
+    stateSave.push({
+      title: val,
+      todoStatus: "todo",
+      id: this.state.globalid + 1
+    });
 
-    appendToList(val) {
-        let stateSave = this.state.list
-        stateSave.push({
-            title: val,
-            todoStatus: 'todo',
-            id: this.state.globalid + 1
-        })
+    this.setState({
+      globalid: this.state.globalid + 1,
+      list: stateSave
+    });
+  }
 
-
-        this.setState({
-            globalid: this.state.globalid + 1,
-            list: stateSave
-        })
-    }
-
-
-    render() {
-        let input
-        return (
-            <div>
-                <input type="text" ref={(a) => input = a} />
-                <button onClick={() => {
-                    this.appendToList(input.value);
-                    input.value = '';
-
-                }}>
-                    go
-                </button>
-                <TodoList list={this.state.list} />
-
-            </div >
-        )
-
-    }
+  render() {
+    let input;
+    return (
+      <div>
+        <input type="text" ref={a => (input = a)} />
+        <button
+          onClick={() => {
+            this.appendToList(input.value);
+            input.value = "";
+          }}
+        >
+          go
+        </button>
+        <TodoList list={this.state.list} />
+      </div>
+    );
+  }
 }
--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,21 +1,26 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 class Todo extends Component {
-    constructor(props) {
-        super(props)
-        this.state = { done: false }
-        this.changeColor = this.changeColor.bind(this)
-    }
-    changeColor() {
-        this.setState({ done: true })
-    }
-    render() {
-        let itemStatus = this.state.done ? "done" : "todo"
-        return (
-            <li className={itemStatus} key={this.props.idf} onClick={this.changeColor}>
-                {this.props.text}
-            </li>
-        )
-    }
+  constructor(props) {
+    super(props);
+    this.state = { done: false };
+  }
+
+  changeColor = () => {
+    this.setState({ done: true });
+  }
+
+  render() {
+    let itemStatus = this.state.done ? "done" : "todo";
+    return (
+      <li
+        className={itemStatus}
+        key={this.props.idf}
+        onClick={this.changeColor}
+      >
+        {this.props.text}
+      </li>
+    );
+  }
 }
-export default Todo
+export default Todo;
